711a4c3302d76c4f58ae49f02a683bcc9db45170
shorinkiwata <fcl709@outlook.com>
2026-03-18 22:17:06 +0900

    常用软件安装新增virt-viewer

--- scripts/99-apps.sh
+++ scripts/99-apps.sh
@@ -278,7 +278,7 @@ if pacman -Qi virt-manager &>/dev/null && ! systemd-detect-virt -q; then
   # 1. 安装完整依赖
   # iptables-nft 和 dnsmasq 是默认 NAT 网络必须的
   log "Installing QEMU/KVM dependencies..."
-  pacman -S --noconfirm --needed qemu-full virt-manager swtpm dnsmasq 
+  pacman -S --noconfirm --needed qemu-full virt-manager swtpm dnsmasq virt-viewer
 
   # 2. 添加用户组 (需要重新登录生效)
   log "Adding $TARGET_USER to libvirt group..."
